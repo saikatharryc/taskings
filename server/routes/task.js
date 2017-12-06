a83edf4eb6ade5dfc7a652e4ecf8d80f51e5811b
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2017-12-06 22:15:04 +0530

    updated status

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,26 +1,27 @@
 var tasks = require('../controllers/tasks');
 var express = require('express');
+var http_status = require('http-status');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
 	tasks.getAll(req).then(data => {
 		return res.json(data);
 	}).catch(err => {
-		return next({ message: "some error Occured", err: err });
+		return next({ message: "Cannot Get!", err: err, status: http_status.BAD_REQUEST });
 	});
 });
 router.post('/create', (req, res, next) => {
 	tasks.addTask(req, req.body).then(data => {
 		return res.json(data);
 	}).catch(err => {
-		return next({ message: "some error Occured", err: err });
+		return next({ message: "Cannot Create!", err: err, status: http_status.BAD_REQUEST });
 	});
 });
 router.post('/delete', (req, res, next) => {
 	tasks.deleteTask(req).then(data => {
 		return res.json(data);
 	}).catch(err => {
-		return next({ message: "some error Occured", err: err });
+		return next({ message: "Cannot delete!", err: err, status: http_status.BAD_REQUEST });
 	});
 });
 
