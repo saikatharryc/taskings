4836eebd4125355c58f364c8b387d245f890f0f2
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2017-12-06 22:00:34 +0530

    removed auto unnecessery  package

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -13,8 +11,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 var routes = require('./server/routes/index');
 routes.includeRoutes(app);
 
