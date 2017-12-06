e861a8629c2b8919832459501e16f3e0b1cc0b71
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2017-12-06 22:11:34 +0530

    schema change

--- server/models/task.js
+++ server/models/task.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var TaskSchema = new mongoose.Schema({
 	task_name: String,
 	description: String,
-	amount: String
+	amount: Number
 }, { timestamps: true });
 
 // TaskSchema.path('task_name').required(true, 'Task name cannot be blank');
